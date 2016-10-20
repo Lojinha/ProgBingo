b7c42660fcd3a11fe53516f905202199ad306c61
Lojinha <ricardo.poncio@aluno.positivo.edu.br>
2016-10-20 16:26:54 -0200

    Add files via upload

--- BingoServidor.c
+++ BingoServidor.c
@@ -1,121 +1,149 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
 #include <string.h>
 #include <unistd.h>
 
 #ifdef __WIN32
 	#include <winsock2.h>
 	#include <conio.h>
+	WSADATA data; 
+	SOCKET sockfd; 
+	SOCKADDR_IN serverAddr;
+	//linker -> -l wsock32
 #elif __linux__
 	#include <sys/types.h>
 	#include <sys/socket.h>
 	#include <netinet/in.h>
 	#include <arpa/inet.h>
 	#include <netdb.h>
+	int sockfd;
+	struct sockaddr_in serverAddr;
+	void closesocket(int socket) { close(socket); }
+	//linker -> -lws2_32 -lwsock32 -L $MinGW\lib
 #endif
 
-#define	MAX_CON	10
-#define MAX_SIZE 50
-#define NUM_CLIENT 5
+#define		MAX_CON		1
+#define 	MAX_SIZE 	50
+#define 	NUM_CLIENT 	5
+#define		PORTA		8188
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int socket_desc , client_sock , c , *new_sock;
-struct sockaddr_in server , client;
+int conexao() {
+	if( accept(sockfd, 0, 0) == -1)
+		return 0;
+	else
+		return 1;
+}
+
+int* retornaCartela() {
+	int *cartela = (int*)malloc(26);
+	int i, j, numerosDiversos = 0, numeroGerado;
+	while(numerosDiversos < 25) {
+		if(numerosDiversos == 12) {
+			cartela[numerosDiversos] = 0;
+			numerosDiversos++;
+			continue;
+		}
+		numeroGerado = randomInteger(1, 99);
+		for(j = 0; j < numerosDiversos; j++)
+			if(cartela[j] == numeroGerado)
+				break;
+		if(j == numerosDiversos) {
+			cartela[numerosDiversos] = numeroGerado;
+			numerosDiversos++;
+		}
+	}
+	return cartela;
+}
+
+int* ordenaCrescente(int* cartela) {
+	int *cartelaAuxiliar;
+	cartelaAuxiliar = cartela;
+	return cartelaAuxiliar;
+}
+
+int randomInteger (int low, int high) {
+    int k;
+    double d;
+    d = (double) rand () / ((double) RAND_MAX + 1);
+    k = d * (high - low + 1);
+    return low + k;
+}
+
+void printaCartela(int* cartela) {
+	int i, j, aux;
+	
+	for(i = 0; i < 25; i++) {
+		if(cartela[i] == 0) {
+			if(i%5==0) {
+				printf("|	-	|", cartela[i]);
+			} else if((i+1)%5==0) {
+				printf("	-	|\n", cartela[i]);
+			} else {
+				printf("	-	|", cartela[i]);
+			}
+			continue;
+		}
+		if(i%5==0) {
+			printf("|	%d	|", cartela[i]);
+		} else if((i+1)%5==0) {
+			printf("	%d	|\n", cartela[i]);
+		} else {
+			printf("	%d	|", cartela[i]);
+		}
+	}
+}
 
 int configuracaoServidor()
 {
-    int sockfd;
-    struct sockaddr_in serverAddr;
+	#ifdef __WIN32
+		WSADATA data; 
+		SOCKET sockfd; 
+		SOCKADDR_IN serverAddr;
+		if(WSAStartup(MAKEWORD(1,1),&data)==SOCKET_ERROR){ 
+			printf("Erro ao inicializar o winsock"); 
+			return -1;
+		}
+	#elif __linux__
+		int sockfd;
+    	struct sockaddr_in serverAddr;
+	#endif
+	
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
       return -1;
     memset(&serverAddr, 0, sizeof (serverAddr));
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serverAddr.sin_port = htons(1234);
-    if (bind(sockfd, (struct sockaddr *) & serverAddr, sizeof (serverAddr)) < 0)
+    serverAddr.sin_port = htons(PORTA);
+    if (bind(sockfd, (struct sockaddr *) & serverAddr, sizeof(serverAddr)) < 0)
     	return -1;
     if (listen(sockfd, MAX_CON) < 0)
     	return -1;
     return sockfd;
 }
 
-void *thread_Serial(void *arg) {
-    char buffer_do_cliente[256];
-    int sockEntrada = *(int *) arg;
-    printf("Aguardando as mensagens... ");
-    for (;;) {
-        read(sockEntrada, buffer_do_cliente, sizeof (buffer_do_cliente));
-        if (strcmp(buffer_do_cliente, "sair") != 0) {
-            printf("%s\n",buffer_do_cliente);
-        }
-        else {
-        	closesocket(sockEntrada);
-        	pthread_exit((void*) 0);
-        }
-    }
-}
-
-void *thread_teste(void *threadid) {
-    int threadnum = (int)threadid;
-    int sock_desc;
-    struct sockaddr_in serv_addr;
-    char sbuff[MAX_SIZE],rbuff[MAX_SIZE];
-	
-	sock_desc = configuracaoServidor();
-    while(accept(sock_desc, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0);
-
-    printf("Connected successfully client:%d\n", threadnum);
-    while(1)
-    {
-        if(recv(sock_desc,rbuff,MAX_SIZE,0)==0)
-            printf("Error");
-        else
-           fputs(rbuff,stdout);
-        memset(rbuff,MAX_SIZE, sizeof(rbuff));
-        sleep(2);
-    }
-    close(sock_desc);
-    return 0;
-}
-
 int main(int argc, char *argv[]) {
-    struct sockaddr_in serverAddr;
-    int sockfd = configuracaoServidor();
-      
-//	int i;
-//	pthread_t sniffer_thread;
-//    for (i=1; i<=NUM_CLIENT; i++) {
-//        if( pthread_create( &sniffer_thread , NULL ,  thread_teste , (void*) i) < 0)
-//        {
-//            perror("could not create thread");
-//            return 1;
-//        }
-//        sleep(3);
-//    }
-//    pthread_exit(NULL);
-//    return 0;
-   
+	system("mode con:cols=82 lines=20"); //lines=8
+	sockfd = configuracaoServidor();
     if(sockfd == -1) {
       	printf("Erro ao criar Socket\n");
      	exit(1);
 	}
+	printf("Aguardando Conexão!\n");
+	//while(!conexao());
+	printf("Conexão Aceita!\n\n");
 	while (1)
-    {
-        int clienteSockfd;
-        struct sockaddr_in clienteAddr;
-        unsigned int clntLen;
-        clntLen = sizeof (clienteAddr);
-    	pthread_t thread;
-        if ((clienteSockfd = accept(sockfd, (struct sockaddr *) & clienteAddr, &clntLen)) < 0) {
-      		printf("Erro no Socket\n");
-     		exit(1);
-    	}
-        pthread_create(&thread, NULL, thread_Serial, &clienteSockfd) != 0;
-        pthread_detach(thread);
+    {   	
+		int *cartela;
+		cartela = retornaCartela();
+		//printaCartela(cartela);
+		//printf("\n\n");
+		//cartela = ordenaCrescente(cartela);
+		//printaCartela(cartela);
+		//break;
     }
-
+	getchar();
+	closesocket(sockfd);
 	return 0;
 }
